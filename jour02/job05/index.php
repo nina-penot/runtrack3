663a4c1fff12e2af60d9748e56f0c61865dbbe0f
nina-penot <nina.penot@laplateforme.io>
2025-12-05 15:23:10 +0100

    rendu jour02 job05

--- jour02/job05/index.php
+++ jour02/job05/index.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 
-<head></head>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
 
-<body>
-
+<body onscroll="scroller()">
+    <footer id="foot"></footer>
 </body>
 
 <script src="script.js"></script>
---
+++ jour02/job05/script.js
@@ -0,0 +1,48 @@
+var myfooter = document.getElementById("foot"),
+    doc = document.documentElement,
+    body = document.body,
+    st = 'scrollTop',
+    sh = 'scrollHeight';
+
+// myfooter.addEventListener("scroll", function (e) {
+//     console.log("hey!!");
+// })
+// console.log(doc);
+// body.addEventListener("scroll", function (e) {
+//     console.log("done");
+// })
+
+function scroller() {
+    //Récupre le pourcentage en utilisant les propriétés "scrolltop" et "scrollheight"
+    //de body et doc html
+    var percent = (doc[st] || body[st]) / ((doc[sh] || body[sh]) - doc.clientHeight) * 100;
+    // let colors_wanted = {
+    //     0: "#000000",
+    //     10: "#ff2c2c",
+    //     20: "#ff7d2cff",
+    //     30: "#ffed2cff",
+    //     40: "#64ff2cff",
+    //     50: "#2cff6bff",
+    //     60: "#2ce3ffff",
+    //     70: "#302cffff",
+    //     80: "#d52cffff",
+    //     90: "#ff2caeff",
+    // }
+    // myfooter.style.backgroundColor = "red";
+
+    //Calcule le multiplicateur pour la valeur hue(h) de hsl
+    //310 est choisie comme maximum (rose fushia) (0 est rouge)
+    let mult = 310 / 100;
+    //Construit la couleur en hsl
+    let mycol = "hsl(" + percent * mult + ", 100%, 50%)";
+    //Applique au footer
+    myfooter.style.backgroundColor = mycol;
+    // console.log(mycol);
+
+    // for (i in colors_wanted) {
+    //     if (percent >= i) {
+    //         myfooter.style.backgroundColor = colors_wanted[i];
+    //     }
+    // }
+    // console.log(percent);
+}
