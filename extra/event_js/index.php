3bc92ba977c4a6080bcc3ad7903c1649497e7df8
nina-penot <nina.penot@laplateforme.io>
2025-12-05 13:13:59 +0100

    Ajout extra event_js, ajout jour02

---
+++ extra/event_js/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<head>
+</head>
+
+<body>
+    <button id="monBouton">Clique-moi</button>
+    <p id="message"></p>
+</body>
+
+<script src="script.js"></script>
---
+++ extra/event_js/script.js
@@ -0,0 +1,20 @@
+// Attendre que le DOM soit chargé
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Sélectionner le bouton
+    let bouton = document.getElementById('monBouton');
+
+    // Ajouter un écouteur d'événement
+    bouton.addEventListener('mousedown', function () {
+        let message = document.getElementById('message');
+        message.textContent = 'Tu as cliqué !';
+        message.style.color = '#E85D10';
+        message.style.fontSize = '1.3em';
+    });
+
+    bouton.addEventListener('mouseup', function () {
+        let message = document.getElementById('message');
+        message.textContent = "";
+    })
+
+});
