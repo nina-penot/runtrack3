aa6bcfc27619cf6153144911ec0426f25e4fd6e4
nina-penot <nina.penot@laplateforme.io>
2025-12-08 14:58:51 +0100

    petite amélioration jour02 job04
    "e.key * 2" au lieu de "e.key + e.key"

--- jour02/job04/script.js
+++ jour02/job04/script.js
@@ -4,5 +4,5 @@ window.addEventListener('keydown', function (e) {
     e.preventDefault();
     // keylogger.value += e.key;
     keylogger.value = document.activeElement.id == "keylogger" ?
-        keylogger.value + e.key + e.key : keylogger.value + e.key;
+        keylogger.value + (e.key * 2) : keylogger.value + e.key;
 })
