9abe7ffcf72241030478ae7dc6c4d08f9074979b
nina-penot <nina.penot@laplateforme.io>
2025-12-17 14:43:51 +0100

    rendu jour08 job02

--- jour08/job02/index.php
+++ jour08/job02/index.php
@@ -1,62 +1,67 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>Document</title>
 </head>
 
 <body>
-    <header>
-        <nav>
-            <a href="">Home</a>
-            <a href="">1</a>
-            <a href="">2</a>
+    <header class="bg-sky-500 p-3">
+        <div class="flex">
+            <div class="inline-block bg-amber-400 p-5 rounded-3xl">LOGO</div>
+            <div class="text-6xl bg-blue-600 self-center ml-5 pl-5 pr-5 pb-2 pt-2 rounded-2xl">TITRE</div>
+        </div>
+
+        <nav class="p-3 bg-sky-400 rounded-2xl mt-2">
+            <a class="rounded-2xl bg-sky-600 p-3 hover:bg-amber-400 inline-block min-w-24" href="">Home</a>
+            <a class="rounded-2xl bg-sky-600 p-3 hover:bg-amber-400 inline-block min-w-24" href="">1</a>
+            <a class="rounded-2xl bg-sky-600 p-3 hover:bg-amber-400 inline-block min-w-24" href="">2</a>
         </nav>
     </header>
 
     <main>
-        <section>
+        <section class="m-3">
             <form method="post">
                 <div>
                     <p>Civilité</p>
-                    <input type="radio" name="civil" id="mr" value="monsieur">
+                    <input type="radio" name="civil" id="mr" value="monsieur" />
                     <label for="mr">Monsieur</label>
-                    <input type="radio" name="civil" id="ms" value="madame">
+                    <input type="radio" name="civil" id="ms" value="madame" />
                     <label for="ms">Madame</label>
-                    <input type="radio" name="civil" id="other" value="autre">
+                    <input type="radio" name="civil" id="other" value="autre" />
                     <label for="other">Autre</label>
                 </div>
 
                 <div>
                     <label for="name">Prénom</label>
-                    <input type="text" name="name" id="name">
+                    <input type="text" name="name" id="name" />
                     <label for="lastname">Nom</label>
-                    <input type="text" name="lastname" id="lastname">
+                    <input type="text" name="lastname" id="lastname" />
                 </div>
 
                 <label for="adress">Adresse</label>
-                <input type="text" name="adress" id="adress">
+                <input type="text" name="adress" id="adress" />
 
                 <label for="email">Email</label>
-                <input type="email" name="email" id="email">
+                <input type="email" name="email" id="email" />
 
                 <label for="pass">Mot de passe</label>
-                <input type="password" name="pass" id="pass">
+                <input type="password" name="pass" id="pass" />
                 <label for="pass_ver">Vérifiez le mot de passe</label>
-                <input type="password" name="pass_ver" id="pass_ver">
+                <input type="password" name="pass_ver" id="pass_ver" />
 
                 <div>
                     <p>Choisissez vos passions</p>
-                    <input type="checkbox" name="informatique" id="informatique" value="informatique">
+                    <input type="checkbox" name="informatique" id="informatique" value="informatique" />
                     <label for="informatique">Informatique</label>
-                    <input type="checkbox" name="sport" id="sport" value="sport">
+                    <input type="checkbox" name="sport" id="sport" value="sport" />
                     <label for="sport">Sport</label>
-                    <input type="checkbox" name="lecture" id="lecture" value="lecture">
+                    <input type="checkbox" name="lecture" id="lecture" value="lecture" />
                     <label for="lecture">Lecture</label>
-                    <input type="checkbox" name="voyages" id="voyages" value="voyages">
+                    <input type="checkbox" name="voyages" id="voyages" value="voyages" />
                     <label for="voyages">Voyages</label>
                 </div>
 
@@ -65,12 +70,12 @@
         </section>
     </main>
 
-    <footer>
-        <ul>
-            <li><a href="">Accueil</a></li>
-            <li><a href="">Inscription</a></li>
-            <li><a href="">Connexion</a></li>
-            <li><a href="">Rechercher</a></li>
+    <footer class="bg-sky-500 p-3">
+        <ul class="gap-4">
+            <li class="rounded-2xl bg-sky-600 p-3 hover:bg-blue-800 mb-3 w-3/4"><a class="bg-amber-50" href="">Accueil</a></li>
+            <li class="rounded-2xl bg-sky-600 p-3 hover:bg-blue-800 mb-3"><a href="">Inscription</a></li>
+            <li class="rounded-2xl bg-sky-600 p-3 hover:bg-blue-800 mb-3"><a href="">Connexion</a></li>
+            <li class="rounded-2xl bg-sky-600 p-3 hover:bg-blue-800 "><a href="">Rechercher</a></li>
         </ul>
     </footer>
 </body>
