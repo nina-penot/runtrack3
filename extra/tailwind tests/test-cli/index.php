e6de04998674d2a1a59ca99ff2770cab7105c7b6
nina-penot <nina.penot@laplateforme.io>
2025-12-18 12:57:29 +0100

    Ajout exercise installation tailwind cli

---
+++ extra/tailwind tests/test-cli/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/app.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="bg-blue-500 border text-4xl">hello</header>
+
+    <button class="bg-red-400 cursor-pointer p-3 ">HELLO?</button>
+
+</body>
+
+</html>
