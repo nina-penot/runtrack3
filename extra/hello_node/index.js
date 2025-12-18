845f151b9a1c7534e6d3d381ad1d7919bb7596d2
nina-penot <nina.penot@laplateforme.io>
2025-12-18 16:40:30 +0100

    Ajout jour08/job05 (bonus) + nodejs init

---
+++ extra/hello_node/index.js
@@ -0,0 +1,3 @@
+console.log("Hello Node.js !");
+const version = process.version;
+console.log("Version of NodeJS : ", version);
---
+++ jour08/job05/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="bg-black">
+        <nav class="text-white p-2.5 mb-3 pb-3">
+            <a class="p-2 hover:border-b-red-500 hover:border-b-4" href="">JPL</a>
+            <a class="p-2 hover:border-b-red-500 hover:border-b-4" href="">Careers</a>
+            <a class="p-2 hover:border-b-red-500 hover:border-b-4" href="">Education</a>
+            <a class="p-2 hover:border-b-red-500 hover:border-b-4" href="">Science & Technology</a>
+        </nav>
+    </header>
+
+    <main>
+        <div class="sticky">
+            <div id="logogroup">
+
+            </div>
+            <nav>
+
+            </nav>
+
+        </div>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
