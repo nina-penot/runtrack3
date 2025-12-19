e80185bb5ee10f5eec777fe21d9705ff2e117d85
nina-penot <nina.penot@laplateforme.io>
2025-12-19 11:46:20 +0100

    Renommage expr_index => index.js

---
+++ extra/node/calcul-node/index.js
@@ -0,0 +1,14 @@
+//index.js
+const nombres = [1, 2, 3, 4, 5];
+
+//somme
+const somme = nombres.reduce((acc, n) => acc + n, 0);
+console.log("Somme : ", somme);
+
+//moyenne
+const moyenne = somme / nombres.length;
+console.log("Moyenne : ", moyenne);
+
+//filter > 3
+const plusDeTrois = nombres.filter(n => n > 3);
+console.log("Nombres > 3 : ", plusDeTrois);
