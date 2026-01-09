43cc68a63854acf9e5d8b20463e5d46872d5bcd7
nina-penot <nina.penot@laplateforme.io>
2026-01-09 10:56:27 +0100

    Réveil + minuteur
    - Rajout sur la page pour le minuteur des inputs heure, minute et seconde
    - Rajout sur la page minuteur (timer) d'un boutton démarrer
    - WIP: Essais pour faire en sorte que les inputs révéil n'acceptent que des chiffres uniquement
    - Début de code pour le minuteur

--- oclock/js/alarm.js
+++ oclock/js/alarm.js
@@ -4,6 +4,9 @@ const alarm_hour = easy_id_get("alarm_hour"),
 
 const button = easy_id_get("set_alarm");
 
+const allowed = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 if (load_alarms()) {
     let saved_alarms = load_alarms();
     for (i = 0; i < saved_alarms.length; i++) {
@@ -14,21 +17,39 @@ if (load_alarms()) {
 }
 
 alarm_hour.addEventListener("input", (e) => {
-    if (alarm_hour.toString().length < 2) {
-        alarm_hour.value = make_double_digit(alarm_hour.value)
+    if (!allowed.includes(e.data) && e.data != null && Number(e.data) < 10) {
+        e.preventDefault();
+        alarm_hour.value -= alarm_hour[-1];
+    } else {
+        if (alarm_hour.toString().length < 2 && alarm_hour.value != "") {
+            alarm_hour.value = make_double_digit(alarm_hour.value)
+        }
+        if (alarm_hour.value > 23) {
+            alarm_hour.value = 23;
+        }
+        if (alarm_hour.value < 0) {
+            alarm_hour.value = 0;
+        }
     }
 });
 
 alarm_min.addEventListener("input", (e) => {
-    let allowed = ["1", "2", "3", "4", "5", "6", "7", "8", "9",
-        1, 2, 3, 4, 5, 6, 7, 8, 9];
-    if (!allowed.includes(e.data)) {
-        e.preventDefault();
-        console.log(":(")
+
+    // console.log(alarm_min.value);
+    console.log(e.data);
+    if (!allowed.includes(e.data) && e.data != null && Number(e.data) < 10) {
+        // e.preventDefault();
+        // console.log(alarm_min.value)
+        console.log(alarm_min.value);
+        let test = alarm_min.value.substring(0, alarm_min.value.length - 1);
+        alarm_min.value = test;
+
+        console.log(alarm_min.value + " " + test)
     } else {
-        if (alarm_min.toString().length < 2 && alarm_min.value != "") {
-            alarm_min.value = make_double_digit(alarm_min.value)
-        }
+        // if (alarm_min.value.toString().length < 2 && alarm_min.value != "") {
+        //     console.log("len < 2")
+        //     alarm_min.value = make_double_digit(alarm_min.value)
+        // }
         if (alarm_min.value > 59) {
             alarm_min.value = 59;
         }
@@ -37,7 +58,6 @@ alarm_min.addEventListener("input", (e) => {
         }
     }
 
-    console.log(alarm_min.value);
 });
 
 button.addEventListener("click", (e) => {
@@ -51,6 +71,8 @@ button.addEventListener("click", (e) => {
     if (typeof Number(alarm_min.value) != "number" || typeof Number(alarm_hour.value) != "number") {
         console.log("error: not a number");
     } else {
+        alarm_hour.value = make_double_digit(alarm_hour.value);
+        alarm_min.value = make_double_digit(alarm_min.value);
         let myid = save_alarm(alarm_hour.value, alarm_min.value);
         list_elem = create_alarm_elem(myid, alarm_hour.value, alarm_min.value);
         //set it to activate at time and have a button to deactivate
---
+++ oclock/js/timer.js
@@ -0,0 +1,7 @@
+const myinputs = easy_class_get("timer_input");
+const my_inp_groups = easy_class_get("timer_group");
+
+//let test = easy_find_child(my_inp_groups[1], "button");
+console.log(my_inp_groups.length)
+
+let btn_targetting = { "btn": "", "target": "" };
