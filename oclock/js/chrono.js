02bc1fb94f9067f34ac02a811986083f52da0d02
nina-penot <nina.penot@laplateforme.io>
2026-01-09 16:07:57 +0100

    Fini: chronomètre
    - Bouton reset met le chrono à 0
    - Bouton tour sauvegarde correctement le temps du chrono actuel

--- oclock/js/chrono.js
+++ oclock/js/chrono.js
@@ -1,10 +1,12 @@
 const chrono = easy_id_get("chrono_display");
+const chrono_list = easy_id_get("chrono_list");
 
 const start_btn = easy_id_get("start_stop"),
     save_btn = easy_id_get("save_chrono"),
     reset_btn = easy_id_get("reset");
 
 let pressed = false;
+let saved_time = 0;
 
 let myinterval;
 
@@ -16,12 +18,13 @@ start_btn.addEventListener("click", (e) => {
 
     if (pressed) {
         myinterval = setInterval(() => {
-            // time = time++;
-            // chrono.textContent = new Date(time).toISOString().slice(11, 21);
-            chrono.textContent = Date.now() - now;
-        }, 100);
+            mytime = Date.now() - now + saved_time;
+            thistime = new Date(Date.now() - now + saved_time).toISOString().slice(11, 22);
+            chrono.textContent = thistime;
+        }, 1);
     } else {
         clearInterval(myinterval);
+        saved_time = mytime;
     }
 
 });
@@ -29,8 +32,11 @@ start_btn.addEventListener("click", (e) => {
 reset_btn.addEventListener("click", (e) => {
     //put chrono to 0
     clearInterval(myinterval);
+    chrono.textContent = new Date(0).toISOString().slice(11, 22);
 });
 
 save_btn.addEventListener("click", (e) => {
     //save current chrono to the list
+    let timesave = easy_quick_create("div", "alarm_time", chrono.textContent);
+    easy_append_children(chrono_list, timesave);
 });
